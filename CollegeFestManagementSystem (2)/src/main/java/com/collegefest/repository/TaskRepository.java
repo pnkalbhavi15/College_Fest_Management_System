cb4cffd70963478f990560de717b92741a4d41f7
prayashi447 <118040629+prayashi447@users.noreply.github.com>
2025-04-20 12:17:13 +0530

    Task Added in Volunteer corrected

--- CollegeFestManagementSystem (2)/src/main/java/com/collegefest/repository/TaskRepository.java
+++ CollegeFestManagementSystem (2)/src/main/java/com/collegefest/repository/TaskRepository.java
@@ -3,11 +3,10 @@ package com.collegefest.repository;
 import com.collegefest.model.Task;
 import com.collegefest.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+
 
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
-    @Query("SELECT t FROM Task t JOIN FETCH t.event")
     List<Task> findByEvent(Event event);
 }
