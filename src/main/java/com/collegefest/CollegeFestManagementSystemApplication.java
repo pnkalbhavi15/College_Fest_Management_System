f43d3d09b9a9f189733d3115745ebc382a6bed6c
Praneetha Kalbhavi <praneethakalbhavi@gmail.com>
2025-10-25 23:12:55 +0530

    Update Spring Boot DB configurations to use environment variables

--- src/main/java/com/collegefest/CollegeFestManagementSystemApplication.java
+++ src/main/java/com/collegefest/CollegeFestManagementSystemApplication.java
@@ -2,10 +2,17 @@ package com.collegefest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class CollegeFestManagementSystemApplication {
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load(); // loads .env file
+
+        System.setProperty("JDBC_DATABASE_URL", dotenv.get("JDBC_DATABASE_URL"));
+        System.setProperty("JDBC_DATABASE_USERNAME", dotenv.get("JDBC_DATABASE_USERNAME"));
+        System.setProperty("JDBC_DATABASE_PASSWORD", dotenv.get("JDBC_DATABASE_PASSWORD"));
+
         SpringApplication.run(CollegeFestManagementSystemApplication.class, args);
     }
 }
